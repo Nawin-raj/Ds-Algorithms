50d15faef5ba0e0860b3f0a2364ee2fdfa0a7d65
Nawin-raj <43126614+Nawin-raj@users.noreply.github.com>
2020-05-26 11:08:49 +0530

    Create CheckBST.java
    Program to check whether a tree is BST or not

---
+++ BinaryTrees/CheckBST.java
@@ -0,0 +1,42 @@
+package bst;
+
+import bst.BST.Node;
+
+/** we can do this using two ways
+ * Do In-Order Traversal of the given tree and store the result in a temp array.
+ * Check if the temp array is sorted in ascending order, if it is, then the tree is BST.
+ * Bcoz inorder traversal gives sorted output  Time Complexity: O(n)
+ * 
+ * 
+ *
+ */
+public class CheckBST {
+
+	public static void main(String[] args) {
+		BST b1 = new BST();
+		b1.insertdata(1);
+		b1.insertdata(0);
+		b1.insertdata(-1);
+		b1.insertdata(-2);
+		b1.insertdata(3);
+		b1.insertdata(2);
+		boolean flag = checkbst(b1.root);
+		System.out.println(flag);
+		System.out.println(Integer.MAX_VALUE);
+
+	}
+
+	public static boolean checkbst(Node root) {
+		return isBST(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+	}
+
+	public static boolean isBST(Node root, int min, int max) {
+		if (root == null)
+			return true;
+		if (root.data < min || root.data > max)
+			return false;
+
+		return isBST(root.left, min, root.data) && isBST(root.right, root.data, max);
+	}
+
+}
