115ffdbe09e1aa5444c15f43d0dffcbd1d9e7c70
Nawin-raj <43126614+Nawin-raj@users.noreply.github.com>
2020-08-29 16:32:15 +0530

    Update CheckBST.java

--- BinaryTrees/CheckBST.java
+++ BinaryTrees/CheckBST.java
@@ -6,8 +6,8 @@ import bst.BST.Node;
  * Do In-Order Traversal of the given tree and store the result in a temp array.
  * Check if the temp array is sorted in ascending order, if it is, then the tree is BST.
  * Bcoz inorder traversal gives sorted output  Time Complexity: O(n)
- * 
- * 
+ *
+ * iterative solution is some what difficult to understand.
  *
  */
 public class CheckBST {
@@ -23,6 +23,7 @@ public class CheckBST {
 		boolean flag = checkbst(b1.root);
 		System.out.println(flag);
 		System.out.println(Integer.MAX_VALUE);
+		System.out.println(usingcomparison(b.root));
 
 	}
 
@@ -37,6 +38,26 @@ public class CheckBST {
 			return false;
 
 		return isBST(root.left, min, root.data) && isBST(root.right, root.data, max);
+	}
+	
+	//using comparison
+		private static boolean usingcomparison(Node node) {
+		if (node == null) {
+			return true;
+		}
+		boolean returnvalue = usingcomparison(node.left);
+		if (!returnvalue) {
+			return false;
+		}
+
+		if (prev != null && node.data < prev.data) {
+			return false;
+		}
+
+		prev = node;
+
+		return usingcomparison(node.right);
+
 	}
 
 }
